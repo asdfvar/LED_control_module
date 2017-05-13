f34e5e5587c7d49820b3c5cceebf53517bbda1e3
asdfvar <asdfvar@gmail.com>
2017-05-13 16:47:58 -0700

    changed variable names

--- src/ss-spectrum-control/light-control.cpp
+++ src/ss-spectrum-control/light-control.cpp
@@ -1,7 +1,7 @@
 #include "menu-system.h"
 #include "lighting-program.h"
 
-static uint16_t desired_intensity;
+static uint16_t z_desired_intensity;
 
 static       WLabel CR_intensity       ( 0,    251, 10  );
 static       WLabel NL_intensity_label ( 2,    251, 60  );
@@ -17,8 +17,8 @@ void WLightControl::paint()
    WLabel::paint(      F("AL INTENSITY"      ),  100, 110, ILI9341_GREEN, ILI9341_BLACK, 2, 0);
    clear_button.paint( F("RESET"),      ILI9341_RED, DARK_COLOR);
 
-   desired_intensity = lp.getDesiredIntensity();
-   CR_intensity.paint_two_digits( desired_intensity, ILI9341_BLACK, ILI9341_WHITE);
+   z_desired_intensity = lp.getDesiredIntensity();
+   CR_intensity.paint_two_digits( z_desired_intensity, ILI9341_BLACK, ILI9341_WHITE);
 
    common_proc();
 
@@ -29,46 +29,46 @@ void WLightControl::touch(uint16_t x, uint16_t y)
 {
    if ( save_button.hit(x, y) )
    {
-      lp.saveLightControl( desired_intensity );
+      lp.saveLightControl( z_desired_intensity );
       lp.restart();
       menu.setMenu( setup_menu );
    } 
    else if ( clear_button.hit(x, y) )
    {
       // reset the intensity level to zero
-      desired_intensity = 0;
-      CR_intensity.paint_two_digits( desired_intensity, ILI9341_BLACK, ILI9341_WHITE );
-      lp.saveLightControl( desired_intensity );
+      z_desired_intensity = 0;
+      CR_intensity.paint_two_digits( z_desired_intensity, ILI9341_BLACK, ILI9341_WHITE );
+      lp.saveLightControl( z_desired_intensity );
    }
    else if ( up_slow.hit(x, y) )
    {
       // increment by 1
-      if ( desired_intensity < 99 ) desired_intensity++;
-      CR_intensity.paint_two_digits( desired_intensity, ILI9341_BLACK, ILI9341_WHITE );
-      lp.saveLightControl( desired_intensity );
+      if ( z_desired_intensity < 99 ) z_desired_intensity++;
+      CR_intensity.paint_two_digits( z_desired_intensity, ILI9341_BLACK, ILI9341_WHITE );
+      lp.saveLightControl( z_desired_intensity );
    }
    else if ( up_fast.hit(x, y) )
    {
       // increment by 10. Max of 99
-      if ( desired_intensity < 89 ) desired_intensity += 10;
-      else                  desired_intensity  = 99;
-      CR_intensity.paint_two_digits( desired_intensity, ILI9341_BLACK, ILI9341_WHITE );
-      lp.saveLightControl( desired_intensity );
+      if ( z_desired_intensity < 89 ) z_desired_intensity += 10;
+      else                  z_desired_intensity  = 99;
+      CR_intensity.paint_two_digits( z_desired_intensity, ILI9341_BLACK, ILI9341_WHITE );
+      lp.saveLightControl( z_desired_intensity );
    }
    else if ( down_fast.hit(x, y) )
    {
       // decrement by 10. Min of 0
-      if ( desired_intensity > 9 ) desired_intensity -= 10;
-      else                 desired_intensity  = 0;
-      CR_intensity.paint_two_digits( desired_intensity, ILI9341_BLACK, ILI9341_WHITE );
-      lp.saveLightControl( desired_intensity );
+      if ( z_desired_intensity > 9 ) z_desired_intensity -= 10;
+      else                 z_desired_intensity  = 0;
+      CR_intensity.paint_two_digits( z_desired_intensity, ILI9341_BLACK, ILI9341_WHITE );
+      lp.saveLightControl( z_desired_intensity );
    }
    else if ( down_slow.hit(x, y) )
    {
       // decrement by 1. Min of 0
-      if ( desired_intensity > 0 ) desired_intensity--;
-      CR_intensity.paint_two_digits( desired_intensity, ILI9341_BLACK, ILI9341_WHITE );
-      lp.saveLightControl( desired_intensity );
+      if ( z_desired_intensity > 0 ) z_desired_intensity--;
+      CR_intensity.paint_two_digits( z_desired_intensity, ILI9341_BLACK, ILI9341_WHITE );
+      lp.saveLightControl( z_desired_intensity );
    }
 }
 
